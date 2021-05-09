6597a93d1426007e2f4e4448af24f513b57ef9bd
jamalwari2 <>
2021-05-09 11:38:26 +0000

    using result in my code

--- app.js
+++ app.js
@@ -51,33 +51,43 @@ while (favGame!="rdr2" && favGame!="fifa21" && favGame != "CODMW" && favGame != 
 }
 // this function is for dispalying the game;
 function favgames(){
+  var result = '';
   // initialize value for result is an empty string;
  
 if (favGame == "rdr2"){
   document.write('<h3 id = "middle">'+ "Your favorite game is "+ '</h3>');
-  document.write('<img src = "https://upload.wikimedia.org/wikipedia/en/4/44/Red_Dead_Redemption_II.jpg" width = "200px" class = "center" >'); 
+  // document.write('<img src = "https://upload.wikimedia.org/wikipedia/en/4/44/Red_Dead_Redemption_II.jpg" width = "200px" class = "center" >'); 
+  result = '<img src = "https://upload.wikimedia.org/wikipedia/en/4/44/Red_Dead_Redemption_II.jpg" width = "200px" class = "center" >'; 
   }else if(favGame == "fifa21"){
 document.write('<h3 id = "middle">'+ "Your favorite game is "+ '</h3>');
-  document.write('<img src = "https://images-na.ssl-images-amazon.com/images/I/81mBk8%2BsxrL._AC_SX385_.jpg"width = "200px" class = "center">');
+  // document.write('<img src = "https://images-na.ssl-images-amazon.com/images/I/81mBk8%2BsxrL._AC_SX385_.jpg"width = "200px" class = "center">');
+   result = '<img src = "https://images-na.ssl-images-amazon.com/images/I/81mBk8%2BsxrL._AC_SX385_.jpg"width = "200px" class = "center">';
 }else if(favGame == "CODMW"){
   document.write('<h3 id = "middle">'+ "Your favorite game is " + '</h3>');
-  document.write('<img src = "https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/kronos/common/social-share/social-share-image.jpg" width = "200px" class = "center">');
+  // document.write('<img src = "https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/kronos/common/social-share/social-share-image.jpg" width = "200px" class = "center">');
+  result = '<img src = "https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/kronos/common/social-share/social-share-image.jpg" width = "200px" class = "center">';
 }else if(favGame == "CODBO"){
 document.write('<h3 id = "middle">'+ "Your favorite game is "+ '</h3>');
-document.write('<img src = "https://upload.wikimedia.org/wikipedia/en/thumb/4/40/Black_Ops_Cold_War.jpeg/220px-Black_Ops_Cold_War.jpeg" width = "200px" class = "center">');
+// document.write('<img src = "https://upload.wikimedia.org/wikipedia/en/thumb/4/40/Black_Ops_Cold_War.jpeg/220px-Black_Ops_Cold_War.jpeg" width = "200px" class = "center">');
+result = '<img src = "https://upload.wikimedia.org/wikipedia/en/thumb/4/40/Black_Ops_Cold_War.jpeg/220px-Black_Ops_Cold_War.jpeg" width = "200px" class = "center">';
 }else if(favGame == "LoUR"){
    document.write('<h3 id = "middle">'+ "Your favorite game is "+ '</h3>');
-  document.write('<img src = "https://image.api.playstation.com/vulcan/img/rnd/202010/2716/Uf86AuvSNRplUW1tpSsYJqQX.png"width = "200px" class = "center">');
+  // document.write('<img src = "https://image.api.playstation.com/vulcan/img/rnd/202010/2716/Uf86AuvSNRplUW1tpSsYJqQX.png"width = "200px" class = "center">');
+  result = '<img src = "https://image.api.playstation.com/vulcan/img/rnd/202010/2716/Uf86AuvSNRplUW1tpSsYJqQX.png"width = "200px" class = "center">';
 }else if(favGame == "Uncharted" ){
   document.write('<h3 id = "middle">'+ "Your favorite game is "+ '</h3>');
-  document.write('<img src = "https://upload.wikimedia.org/wikipedia/en/1/1a/Uncharted_4_box_artwork.jpg"width = "200px" class = "center">');
+  // document.write('<img src = "https://upload.wikimedia.org/wikipedia/en/1/1a/Uncharted_4_box_artwork.jpg"width = "200px" class = "center">');
+  result = '<img src = "https://upload.wikimedia.org/wikipedia/en/1/1a/Uncharted_4_box_artwork.jpg"width = "200px" class = "center">';
 
 }
+return result;
 }
 // Code for raiting
+ var rate = prompt("How many stars (1-5) do you rate "+ favGame +" ?");
 function rateFunc(){
+  var result = '';
  
-  var rate = prompt("How many stars (1-5) do you rate "+ favGame +" ?");
+  // var rate = prompt("How many stars (1-5) do you rate "+ favGame +" ?");
 
 document.write('<h3 id = "middle">'+ "Your Raiting for  "+favGame+"  is"+ '</h3>');
 
@@ -85,59 +95,70 @@ document.write('<h3 id = "middle">'+ "Your Raiting for  "+favGame+"  is"+ '</h3>
 // document.write('<img id = "stars" src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLuPyLWYn_sS2AeIta3BRFYDCKOdcCRbD2Vg&usqp=CAU" width = "50px" class = "center">');
 // }
 if(rate == 1){
-  // result = '<img id = "stars" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Star_rating_1_of_5.png/180px-Star_rating_1_of_5.png" width = "150px" class = "center">';
-document.write('<img id = "stars" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Star_rating_1_of_5.png/180px-Star_rating_1_of_5.png" width = "150px" class = "center">');
+  result = '<img id = "stars" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Star_rating_1_of_5.png/180px-Star_rating_1_of_5.png" width = "150px" class = "center">';
+// document.write('<img id = "stars" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Star_rating_1_of_5.png/180px-Star_rating_1_of_5.png" width = "150px" class = "center">');
 }
 if(rate == 2){
-document.write('<img id = "stars" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Star_rating_2_of_5.png/180px-Star_rating_2_of_5.png" width = "150px" class = "center">');
+// document.write('<img id = "stars" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Star_rating_2_of_5.png/180px-Star_rating_2_of_5.png" width = "150px" class = "center">');
+result = '<img id = "stars" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Star_rating_2_of_5.png/180px-Star_rating_2_of_5.png" width = "150px" class = "center">';
 }
 if(rate == 3){
-document.write('<img id = "stars" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/3_stars.svg/1024px-3_stars.svg.png" width = "150px" class = "center">');
+// document.write('<img id = "stars" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/3_stars.svg/1024px-3_stars.svg.png" width = "150px" class = "center">');
+result = '<img id = "stars" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/3_stars.svg/1024px-3_stars.svg.png" width = "150px" class = "center">';
 }
 
 if(rate == 4){
-document.write('<img id = "stars" src = "https://upload.wikimedia.org/wikipedia/commons/f/fa/Star_rating_4_of_5.png" width = "150px" class = "center">');
+// document.write('<img id = "stars" src = "https://upload.wikimedia.org/wikipedia/commons/f/fa/Star_rating_4_of_5.png" width = "150px" class = "center">');
+result = '<img id = "stars" src = "https://upload.wikimedia.org/wikipedia/commons/f/fa/Star_rating_4_of_5.png" width = "150px" class = "center">';
+
 }
 if(rate == 5){
-document.write('<img id = "stars" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Star_rating_5_of_5.png/180px-Star_rating_5_of_5.png" width = "150px" class = "center">');
+// document.write('<img id = "stars" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Star_rating_5_of_5.png/180px-Star_rating_5_of_5.png" width = "150px" class = "center">');
+result = '<img id = "stars" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Star_rating_5_of_5.png/180px-Star_rating_5_of_5.png" width = "150px" class = "center">';
 }
+return result
+} 
+
+function printingImagesDependingOnUserInput(){
+  var result = '';
 if(favGame == "rdr2"){
   for (var i = 0 ; i<rate ; i++){
-document.write('<img src = "https://upload.wikimedia.org/wikipedia/en/4/44/Red_Dead_Redemption_II.jpg" width = "200px" class = "center" >');
+result += '<img src = "https://upload.wikimedia.org/wikipedia/en/4/44/Red_Dead_Redemption_II.jpg" width = "200px" class = "center" >';
 }
 }
 if(favGame == "fifa21"){
   for (var i = 0 ; i<rate ; i++){
-document.write('<img src = "https://images-na.ssl-images-amazon.com/images/I/81mBk8%2BsxrL._AC_SX385_.jpg"width = "200px" class = "center">');
+result += '<img src = "https://images-na.ssl-images-amazon.com/images/I/81mBk8%2BsxrL._AC_SX385_.jpg"width = "200px" class = "center">';
 }
 }
 if(favGame == "CODMW"){
   for (var i = 0 ; i<rate ; i++){
-document.write('<img src = "https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/kronos/common/social-share/social-share-image.jpg" width = "200px" class = "center">');
+result += '<img src = "https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/kronos/common/social-share/social-share-image.jpg" width = "200px" class = "center">';
   }
 }
 
 if(favGame == "CODBO"){
   for (var i = 0; i<rate ; i++){
-document.write('<img src = "https://upload.wikimedia.org/wikipedia/en/thumb/4/40/Black_Ops_Cold_War.jpeg/220px-Black_Ops_Cold_War.jpeg" width = "200px" class = "center">');
+result += '<img src = "https://upload.wikimedia.org/wikipedia/en/thumb/4/40/Black_Ops_Cold_War.jpeg/220px-Black_Ops_Cold_War.jpeg" width = "200px" class = "center">';
   }
 }
 
 if(favGame == "LoUR"){
   for (var i = 0; i<rate ; i++){
-document.write('<img src = "https://image.api.playstation.com/vulcan/img/rnd/202010/2716/Uf86AuvSNRplUW1tpSsYJqQX.png"width = "200px" class = "center">');
+result += '<img src = "https://image.api.playstation.com/vulcan/img/rnd/202010/2716/Uf86AuvSNRplUW1tpSsYJqQX.png"width = "200px" class = "center">';
   }
 }
 
 if(favGame == "Uncharted"){
   for (var i = 0; i<rate ; i++){
-document.write('<img src = "https://upload.wikimedia.org/wikipedia/en/1/1a/Uncharted_4_box_artwork.jpg"width = "200px" class = "center">');
+result += '<img src = "https://upload.wikimedia.org/wikipedia/en/1/1a/Uncharted_4_box_artwork.jpg"width = "200px" class = "center">';
   }
 }
-
-} 
-favgames();
-rateFunc();
+return result;
+}
+document.write(favgames());
+document.write(rateFunc());
+document.write(printingImagesDependingOnUserInput());
 
 
 
